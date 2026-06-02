3d6540eb31adbb0ad5972fedab90007625bb5b59
emma <ethompson1483@gmail.com>
2026-06-02 08:31:36 +0100

    modified:   admin.html 	modified:   lights.html 	modified:   main.py 	modified:   static/error.mp3 	new file:   static/holding.html 	new file:   static/logo2.png 	modified:   static/password-entry-2.html 	modified:   static/password-entry.html

--- main.py
+++ main.py
@@ -12,45 +12,74 @@ from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
-# Serve everything in /static
-app.mount("/", StaticFiles(directory="static"), name="static")
-
 # State defined as a list
-state = ["red", "red", "red"]
+game_state = [False, False, False]
+game_stage = "holding"
+
+@app.get("/state")
+def get_state():
+    return game_state
+
+@app.post("/set-stage/{stage}")
+def set_stage(stage: str):
+    game_stage = stage
+    return {"status": "ok", "stage": game_stage}
+
+@app.post("/artefact-found/{artefact_id}/{found}")
+def set_the_state_of_an_artefact(artefact_id: int, found: bool):
+    game_state[artefact_id] = found
+    return game_state[artefact_id]
+
+# game_state = {
+#     "stage": stage,
+#     "sliders": {
+#         "slider1": False,
+#         "slider2": False,
+#         "slider3": False
+#     }
+# }
+
+# @app.post("/set-slider/{slider}/{value}")
+# def set_slider(slider: str, value: bool):
+#     if slider in game_state["sliders"]:
+#         game_state["sliders"][slider] = value
+#     return game_state
 
 
-class ColourUpdateSchema(BaseModel):
+
+class ArtefactUpdateSchema(BaseModel):
     """The Schema for updating a colour."""
 
     index: int  # Using integer index for the list
-    new_color: str
+    found: bool
 
-class ColourReturnSchema(BaseModel):
+class ArtefactReturnSchema(BaseModel):
     """The schema for returning a colour."""
 
-    colors: list[str]
+    found: list[bool]
 
 
 # @app.get("/colors") makes it so that you can
 # view the output of get_all_colors in a web browser when you go to /colors.
 @app.get("/colors")
-def get_all_colors() -> ColourReturnSchema:
+def get_all_colors() -> ArtefactReturnSchema:
     """Return all the button states."""
-    return {"colors": state}
+
+    return {"found": game_state}
 
 
-@app.post("/update-color")
-def update_color(updated_colour: ColourUpdateSchema) -> ColourReturnSchema:
-    """Update a colour, given the index and the new colour."""
-    # Ensure the index is within the list range
-    if 0 <= updated_colour.index < len(state):
-        state[updated_colour.index] = updated_colour.new_color
-    else:
-        raise HTTPException(
-            status_code=400,
-            detail=f"Index {updated_colour.index} is out of range.Use 0 to {len(state) - 1}.",  # noqa: E501
-        )
-    return {"colors": state}
+# @app.post("/update-color")
+# def update_color(updated_colour: ColourUpdateSchema) -> ColourReturnSchema:
+#     """Update a colour, given the index and the new colour."""
+#     # Ensure the index is within the list range
+#     if 0 <= updated_colour.index < len(state):
+#         state[updated_colour.index] = updated_colour.new_color
+#     else:
+#         raise HTTPException(
+#             status_code=400,
+#             detail=f"Index {updated_colour.index} is out of range.Use 0 to {len(state) - 1}.",  # noqa: E501
+#         )
+#     return {"colors": state}
 
 
 @app.get("/")
@@ -76,6 +105,9 @@ def show_password_page() -> FileResponse:
 def show_admin_page() -> FileResponse:
     """Return the admin html file."""
     return FileResponse("admin.html")
+
+# Serve everything in /static
+app.mount("/", StaticFiles(directory="static"), name="static")
 
 # If main.py is the application being run, then start a server.
 if __name__ == "__main__":
